d86194d5f9d0a92ed794eeb04799752690de06d2
zany <muguliebe@gmail.com>
2022-06-19 17:02:32 +0900

    add log

--- src/subscriber/mq/topic-log.mq.ts
+++ src/subscriber/mq/topic-log.mq.ts
@@ -5,19 +5,24 @@ const exchange = 'topic.log'
 const queueName = 'topic.log'
 
 export async function initQueue() {
-    log.debug(`topic-log.mq] initQueue start`)
-    const connection = await amqp.connect(process.env.MQ_URL)
-    const channel = await connection.createChannel()
-    await channel.assertExchange(exchange, 'topic')
-    const { queue } = channel.assertQueue(queueName, { durable: true })
-    await channel.bindQueue(queue, queueName)
+    try {
+        log.debug(`topic-log.mq] initQueue start`)
+        const connection = await amqp.connect(process.env.MQ_URL)
+        const channel = await connection.createChannel()
+        await channel.assertExchange(exchange, 'topic')
+        const {queue} = channel.assertQueue(queueName, {durable: true})
+        await channel.bindQueue(queue, queueName)
 
-    channel.consume(queue, async msg => {
-        const content = msg.content
+        channel.consume(queue, async msg => {
+            const content = msg.content
 
-        const tr = JSON.parse(msg.content)
-        log.debug(`topic-log.mq] received: ${msg.fields.routingKey}, ${tr.date}`)
-        channel.ack(msg)
-    })
-    log.debug(`topic-log.mq] initQueue end`)
+            const tr = JSON.parse(msg.content)
+            log.debug(`topic-log.mq] received: ${msg.fields.routingKey}, ${tr.date}`)
+            channel.ack(msg)
+        })
+        log.debug(`topic-log.mq] initQueue end`)
+    }catch (e) {
+        log.error(`when mq initQueue`)
+        throw e
+    }
 }
